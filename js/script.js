8d94cf7dade4e34f52e48f51cc1ea1a1237bfe42
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2022-12-23 16:06:54 +0100

    JS function changed

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@
         console.log("Hello");
     }
 
-    const hideSpecification = () => {
-        const specification = document.querySelector(".js-specification")
+    const updateButtonText = (specification) => {
         const button = document.querySelector(".js-mainSection__button");
 
-        specification.classList.toggle("specification__hide");
         button.innerText = specification.classList.contains("specification__hide") ? "Pokaż specyfikację" : "Ukryj specyfikację";
+    }
+
+    const hideSpecification = () => {
+        const specification = document.querySelector(".js-specification");
+
+        specification.classList.toggle("specification__hide");
+
+        updateButtonText(specification);
     }
 
     const init = () => {
         const button = document.querySelector(".js-mainSection__button");
-        button.addEventListener("click", hideSpecification)
+        button.addEventListener("click", hideSpecification);
 
         welcome();
     };
